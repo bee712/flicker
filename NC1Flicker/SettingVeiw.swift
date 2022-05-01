27f6ea45c7fd39217ebd7602a62d68548cc25d82
bee712 <48711413+bee712@users.noreply.github.com>
2022-05-01 22:35:35 +0900

    Create SettingView

---
+++ NC1Flicker/SettingVeiw.swift
@@ -0,0 +1,20 @@
+//
+//  SettingVeiw.swift
+//  NC1Flicker
+//
+//  Created by Eunbi Han on 2022/05/01.
+//
+
+import SwiftUI
+
+struct SettingVeiw: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingVeiw_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingVeiw()
+    }
+}
