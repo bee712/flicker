1f5812064b6f61767302245b7326a3fa13d16a14
bee712 <48711413+bee712@users.noreply.github.com>
2022-05-01 22:26:15 +0900

    Create Project

---
+++ NC1Flicker/NC1FlickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NC1FlickerApp.swift
+//  NC1Flicker
+//
+//  Created by Eunbi Han on 2022/05/01.
+//
+
+import SwiftUI
+
+@main
+struct NC1FlickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
