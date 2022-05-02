0c42aebb428e51dccb9a71879cf01ad0473b3a2f
bee712 <48711413+bee712@users.noreply.github.com>
2022-05-02 22:39:41 +0900

    Create WriteView

---
+++ WriteView.swift
@@ -0,0 +1,20 @@
+//
+//  WriteView.swift
+//  NC1Flicker
+//
+//  Created by Eunbi Han on 2022/05/02.
+//
+
+import SwiftUI
+
+struct WriteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WriteView_Previews: PreviewProvider {
+    static var previews: some View {
+        WriteView()
+    }
+}
