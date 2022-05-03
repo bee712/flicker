fdb748dcc6c045b548ec017455e060ae89c5b98c
bee712 <48711413+bee712@users.noreply.github.com>
2022-05-03 16:12:33 +0900

    Create SaveView

---
+++ NC1Flicker/SaveView.swift
@@ -0,0 +1,20 @@
+//
+//  SaveView.swift
+//  NC1Flicker
+//
+//  Created by Eunbi Han on 2022/05/03.
+//
+
+import SwiftUI
+
+struct SaveView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SaveView_Previews: PreviewProvider {
+    static var previews: some View {
+        SaveView()
+    }
+}
